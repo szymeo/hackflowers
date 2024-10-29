2ead8b4a0a3015c10780c4a93205e6f0b6d6d8e0
Szymeo <dev.sgracki@gmail.com>
2024-10-29 23:02:56 +0100

    wip: quick commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
 	import '../app.scss';
 	import barefoot from '$lib/assets/PPPAAA.wav';
+	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
+	import { goto } from '$app/navigation';
 
+	const PLAY_INTRO_DELAY = 1300;
 	let { children } = $props();
 	let paused = $state(true);
+	let playingIntro = $state(false);
+
+	onMount(() => {
+		goto('/');
+	});
 </script>
 
 <div class="app relative flex flex-col overflow-hidden">
@@ -14,13 +23,18 @@
 
 	{#if paused}
 		<button
+			out:fade={{ duration: PLAY_INTRO_DELAY }}
 			class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white rounded-full px-6 py-3 tracking-wide font-bold w-fit uppercase text-xs"
 			onclick={() => {
-			paused = false;
-		}}>
+				paused = false;
+				setTimeout(() => {
+					playingIntro = true;
+				}, PLAY_INTRO_DELAY);
+			}}
+		>
 			start
 		</button>
-	{:else}
+	{:else if playingIntro}
 		{@render children()}
 	{/if}
 </div>
